d7af632912ba14306566106b5d7f61029460cc63
Shelby Benfield <benfield.shelby@gmail.com>
2016-12-22 13:41:34 -0500

    Problem works

--- projects/intproblem/problem.py
+++ projects/intproblem/problem.py
@@ -1,17 +1,26 @@
-words = dict()
+# File enter
+name = raw_input('Enter file:')
+# File open, not read
+handle = open(name,'r')
+# File read, one big string
+text = handle.read()
+# File split at spaces, in list now
+words = text.split()
 
-fname = raw_input('Please enter your file name:')
+# Establish dictionary
+counts = dict()
+# Loop through list, add one if there, create and add if not
+for word in words:
+	counts[word] = counts.get(word,0) + 1
 
-fopen = open(fname)
-fread = fopen.read()
-fuppr = fread.upper()
-for line in fuppr:
-	fsplt = fuppr.strip()
-	fword = fsplt.split()
+# Largest so far thing
+bigcount = None
+bigword = None
+# Loop through list, save biggest so far
+for word,count in counts.items():
+	if bigcount is None or count > bigcount:
+		bigword = word
+		bigcount = count
 
-count = dict()
-
-for word in fword:
-	count[word] = count.get(word,0) + 1
-
-print count
+# Print biggest so far
+print bigword,bigcount
